d6e03525985d59f00bc3b9eaace27538e221f362
wyoel <51215426+wyoel@users.noreply.github.com>
2022-11-22 18:48:43 -0500

    Update entrega.php
    conexion api con php

--- entrega.php
+++ entrega.php
@@ -1,3 +1,28 @@
+<?php
+
+$curl = curl_init();
+$dni=$_POST['dni'];
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'http://192.168.100.31:8085/AppiRestFulSFI/api/empleados/'.$dni,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'Authorization: askjdLKJASLK65fd6asa4562ASDasdJJdd152'
+  ),
+));
+
+$response = curl_exec($curl);
+
+//curl_close($curl);
+echo $response;
+
+?> 
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,6 +41,7 @@
   <link rel="stylesheet" href="public/template/css/style.css">
   <!-- endinject -->
   <link rel="shortcut icon" href="public/template/images/favicon.png" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
   <div class="container-scroller d-flex">
@@ -46,18 +72,22 @@
                   <p class="card-description">
                     Cargar datos de Socio
                   </p>
-                  <form class="forms-sample">
+                  <form class="forms-sample" action="entrega.php" method="GET">
                     <div class="form-group">
                         <div class="input-group">
-                        <input type="text" class="form-control" placeholder="DNI">
+                        <input type="text" class="form-control" name="dni" id="dni" placeholder="DNI">
                         <div class="input-group-append">
-                            <button class="btn btn-primary" type="button">Buscar</button>
+                            <button class="btn btn-primary" type="submit">Buscar</button>
                         </div>
                         </div>
                     </div>
                     <div class="form-group">
+                      <label for="">Codigo Agencia</label>
+                      <input type="text" class="form-control" name="codigoAgencia" id="codigoAgencia" value={{$response->codigoAgencia}}>
+                    </div>
+                    <div class="form-group">
                       <label for="">Nombres</label>
-                      <input type="text" class="form-control" id="nombre" placeholder="Nombres">
+                      <input type="text" class="form-control" name="direccion" id="direccion" placeholder="Nombres">
                     </div>
                     <div class="form-group">
                       <label for="">Apellido Paterno</label>
@@ -67,11 +97,7 @@
                       <label for="">Apellido Materno</label>
                       <input type="text" class="form-control" id="apm" placeholder="Apellido Materno">
                     </div>
-                    <div class="form-group">
-                      <label for="">Email address</label>
-                      <input type="email" class="form-control" id="correo" placeholder="Email">
-                    </div>
-                    
+
                     <div class="form-group">
                       <label for="">Genero</label>
                         <select class="form-control" id="exampleSelectGender">
@@ -109,7 +135,7 @@
         <!-- partial:../../partials/_footer.html -->
        
         <!-- partial -->
-        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   
         <!-- content-wrapper ends -->
         <!-- partial:./partials/_footer.html -->
@@ -117,4 +143,5 @@
     </div>
 </body>
 </html>
+
 
