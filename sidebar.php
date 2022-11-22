6cb5f18f1e9cf675a6f7aa785585ec53f335bf40
Ivan Elias Carrasco Lupinta <35948636+ivaneliascalu@users.noreply.github.com>
2022-11-21 22:54:30 -0500

    personalizacion

---
+++ sidebar.php
@@ -0,0 +1,46 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+      <ul class="nav">
+        <li class="nav-item sidebar-category">
+          <p>Navegación</p>
+          <span></span>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">
+            <i class="mdi mdi-view-quilt menu-icon"></i>
+            <span class="menu-title">Panel</span>
+          </a>
+        </li>
+        <li class="nav-item sidebar-category">
+          <p>Modulos</p>
+          <span></span>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="entrega.php">
+            <i class="mdi mdi-palette menu-icon"></i>
+            <span class="menu-title">Entrega</span>
+          </a>
+        </li>
+        
+        <li class="nav-item sidebar-category">
+          <p>Reportes</p>
+          <span></span>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+            <i class="mdi mdi-account menu-icon"></i>
+            <span class="menu-title">Reporte</span>
+            <i class="menu-arrow"></i>
+          </a>
+          <div class="collapse" id="auth">
+            <ul class="nav flex-column sub-menu">
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Reporte Diario </a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/login-2.html"> Reporte General </a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/login-2.html"> Reporte por Zonal </a></li>
+              <li class="nav-item"> <a class="nav-link" href="pages/samples/login-2.html"> Reporte por Oficina </a></li>
+            </ul>
+          </div>
+        </li>
+    
+        
+      </ul>
+    </nav>
