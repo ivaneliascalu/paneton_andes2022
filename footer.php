8782c091d5d62f52b767cb657acb938e43ec83a6
wyoel <51215426+wyoel@users.noreply.github.com>
2022-11-22 09:02:23 -0500

    editar footer

--- footer.php
+++ footer.php
@@ -2,38 +2,13 @@
           <div class="card">
             <div class="card-body">
               <div class="d-sm-flex justify-content-center justify-content-sm-between py-2">
-                <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://www.bootstrapdash.com/" target="_blank">bootstrapdash.com </a>2021</span>
-                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Only the best <a href="https://www.bootstrapdash.com/" target="_blank"> Bootstrap dashboard </a> templates</span>
+                <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://cooperativalosandes.com.pe/" target="_blank"> COOPAC Los Andes </a>2022</span>
+                <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">atencionalsocio@cooperativalosandes.com <a href="https://www.bootstrapdash.com/" target="_blank"> INFORMACIÓN
+                    </a> Jr. Cora Cora nº 319-321 Chalhuanca 945 347 147 
+                </span>
               </div>
             </div>
           </div>
-        </footer>
+</footer>
         <!-- partial -->
-      </div>
-      <!-- main-panel ends -->
-    </div>
-    <!-- page-body-wrapper ends -->
-  </div>
-  <!-- container-scroller -->
-
-  <!-- base:js -->
-  <script src="public/template/vendors/js/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page-->
-  <script src="public/template/vendors/chart.js/Chart.min.js"></script>
-  <script src="public/template/js/jquery.cookie.js" type="text/javascript"></script>
-  <!-- End plugin js for this page-->
-  <!-- inject:js -->
-  <script src="public/template/js/off-canvas.js"></script>
-  <script src="public/template/js/hoverable-collapse.js"></script>
-  <script src="public/template/js/template.js"></script>
-  <!-- endinject -->
-  <!-- plugin js for this page -->
-    <script src="public/template/js/jquery.cookie.js" type="text/javascript"></script>
-  <!-- End plugin js for this page -->
-  <!-- Custom js for this page-->
-  <script src="public/template/js/dashboard.js"></script>
-  <!-- End custom js for this page-->
-</body>
-
-</html>
+      
